6b3efb44b2257c7d42927ddcafdf82f3a7829534
shirafuta <shirafuta.kodai@gmail.com>
2021-12-15 22:14:35 +0900

    CPU completed

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,110 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// if(isKeyPressd)
+//    blackenScreen
+// else
+//    whiten
+// end
+// go back
+
+    
+
+(LOOP)
+    @KBD
+    D=M
+
+    // init i
+    @i
+    M=0
+
+    @BLACKLOOP
+    D;JGT
+    
+    @WHITELOOP
+    0;JEQ
+
+    @LOOP
+    0;JEQ
+
+(BLACKLOOP)
+    // blacken screen
+    // 1row = 16 * 32
+    // screen = 1row * 256
+    // check if @max - @i = 0
+    // if 0 go to end
+    // else proceed
+    
+
+    // set max
+    @8192
+    D=A
+    @max
+    M=D
+
+    // end black loop if reach to end
+    @max
+    D=M
+    @i
+    D=D-M
+    @LOOP
+    D;JEQ
+
+    // select register
+    @SCREEN
+    D=A
+    @i
+    A=M+D
+
+    // set 1 to the row
+    M=-1
+
+    // increment i
+    @i
+    M=M+1
+
+    // go to the beginning
+    @BLACKLOOP
+    0;JEQ
+
+(WHITELOOP)
+    // blacken screen
+    // 1row = 16 * 32
+    // screen = 1row * 256
+    // check if @max - @i = 0
+    // if 0 go to end
+    // else proceed
+    
+
+    // set max
+    @8192
+    D=A
+    @max
+    M=D
+
+    // end black loop if reach to end
+    @max
+    D=M
+    @i
+    D=D-M
+    @LOOP
+    D;JEQ
+
+    // select register
+    @SCREEN
+    D=A
+    @i
+    A=M+D
+
+    // set 1 to the row
+    M=0
+
+    // increment i
+    @i
+    M=M+1
+
+    // go to the beginning
+    @WHITELOOP
+    0;JEQ
+
